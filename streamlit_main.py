987e745bf4dd30ef15cabc21d098e4b5bd5e17f6
Jannik <jannik.elsaesser@gmx.net>
2023-02-23 00:04:30 +0100

    update

--- streamlit_main.py
+++ streamlit_main.py
@@ -28,26 +28,21 @@ import fx
 import base64
 
 @st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
 
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
+
+def set_png_as_page_bg():
     page_bg_img = '''
     <style>
     body {
-    background-image: url("data:image/png;base64,%s");
+    background-image: url("https://unsplash.com/photos/hcvja5GrboU");
     background-size: cover;
     }
     </style>
-    ''' % bin_str
-    
+    '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-set_png_as_page_bg('https://unsplash.com/photos/hcvja5GrboU')
+set_png_as_page_bg()
 
 data = fx.pull_data(days=90)
 
