a7553da662988626de8bbfa53b685ac11952e403
Jannik <jannik.elsaesser@gmx.net>
2023-02-23 00:03:24 +0100

    no

--- streamlit_main.py
+++ streamlit_main.py
@@ -25,6 +25,30 @@ import sys
 sys.path.append('..')
 import fx
 
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('https://unsplash.com/photos/hcvja5GrboU')
+
 data = fx.pull_data(days=90)
 
 # pipelines
@@ -140,7 +164,7 @@ with st.expander("Pipeline diagram:"):
 
 with st.container():
     cola, colb, colc = st.columns(3)
-    button = st.button("Get forecast for next 5 days")
+    button = st.button("Get forecast for the next 5 days from today")
 
     if button:
         with st.spinner("Preparing Data..."):
